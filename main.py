27bcc93d7254a4d4dbd976f547304607be88ecb6
Madeline Arnold <madeline_arnold@berkeley.edu>
2024-11-12 16:30:24 -0800

    updating app.yaml and main.py paths to point to dist directory

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def serve_index():
-    return send_from_directory('.', 'index.html')
+    return send_from_directory('dist', 'index.html')
 
 @app.route('/<path:path>')
 def serve_file(path):
-    return send_from_directory('.', path)
+    return send_from_directory('dist', path)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
